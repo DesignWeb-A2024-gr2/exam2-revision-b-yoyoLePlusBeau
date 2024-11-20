c9e214de77e1237dcfb9408a2acceb5df87f902b
Yoan Bourassa <2442228@carrefour.cegepvicto.ca>
2024-11-20 14:35:39 -0500

    VersionFinale

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,5 +36,60 @@ function isProduitExiste(codeProduit) {
 // Ajoutez votre code plus bas                                             =
 // =========================================================================
 
+const message = document.getElementById("message-erreur");
+const cleActivation = document.getElementById("cle-activation");
+const boite = document.getElementById("declaration");
+const accepte = document.querySelector(".label-declaration");
 
 
+function VerificationCode() {
+    if (REGEX_CODE_PRODUIT.test(cleActivation.value)) {
+        message.style.display = "none";
+        return true;
+    }
+
+    else {
+        message.style.display = "block";
+        message.innerHTML = "Le code produit que vous avez saisie est invalide, il ne respecte pas le format requis.";
+        return false;
+    }
+}
+
+function CleValide() {
+    if (isProduitExiste(cleActivation.value)) {
+        message.style.display = "none";
+        return true;
+    }
+
+    else {
+        message.style.display = "block";
+        message.innerHTML = "Aucun produit n'est associé au code de produit saisie.";
+        return false;
+    }
+}
+
+function Termes() {
+    if (boite.checked) {
+        message.style.display = "none";
+        accepte.style.color = "#c6d4df";
+        return true;
+    }
+
+    else {
+        message.style.display = "block";
+        message.innerHTML = "Vous devez accepter les termes de l’Accord de souscription Vapeur pour finaliser la transaction.";
+        accepte.style.color = "rgb(255, 153, 0)";
+        return false;
+    }
+}
+
+
+function Validation() {
+    if (Termes() && VerificationCode() &&  CleValide()) {
+        return true;
+    }
+ 
+    else {
+        return false;
+    }
+}
